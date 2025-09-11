e25680c56546b577daf1d0abae3c089ae21f0a7e
ekwe7 <ekwea753@gmail.com>
2025-09-11 21:18:42 +0100

    replace first and last later in a string

---
+++ java/FirstLastCharacterString.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class FirstLastCharacterString{
+public static void main(String[] args){
+
+	Scanner scanner = new Scanner(System.in);
+	
+	System.out.print("Enter a word: ");
+	String word = scanner.nextLine();
+	
+	if(word.length() > 0){
+	
+	char firstChar = word.charAt(0);
+	char secondChar = word.charAt(word.length() -1);
+	
+	
+	System.out.println("first char is " + firstChar);
+	System.out.println("second char is " + secondChar);
+
+	}else{
+	System.out.println("Idiot enter a Statement!");
+	}
+
+
+}
+}
