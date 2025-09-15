df214658f18df88fdc0765162fc8b74f39370808
ekwe7 <ekwea753@gmail.com>
2025-09-16 00:11:45 +0100

    char % 2 @str

---
+++ java/EvenIndices.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class EvenIndices{
+public static void main(String[] args){
+
+	Scanner input = new Scanner(System.in);
+	
+	System.out.print("ENter a word: ");
+	String words = input.nextLine();
+	
+	int c = 0;
+	if(c < words.length() && c % 2 == 0){
+	System.out.println(words.charAt(c));
+	} c++;
+	if(c < words.length() && c % 2 == 0){
+	System.out.println(words.charAt(c));
+	}c++;
+	if(c < words.length() && c % 2 == 0){
+	System.out.println(words.charAt(c));
+	}c++;
+	if(c < words.length() && c % 2 == 0){
+	System.out.println(words.charAt(c));
+	}c++;
+	if(c < words.length() && c % 2 == 0){
+	System.out.println(words.charAt(c));
+	}c++;
+	if(c < words.length() && c % 2 == 0){
+	System.out.println(words.charAt(c));
+	}c++;if(c < words.length() && c % 2 == 0){
+	System.out.println(words.charAt(c));
+	}c++;
+if(c < words.length() && c % 2 == 0){
+	System.out.println(words.charAt(c));
+	}c++;
+if(c < words.length() && c % 2 == 0){
+	System.out.println(words.charAt(c));
+	}c++;
+if(c < words.length() && c % 2 == 0){
+	System.out.println(words.charAt(c));
+	}c++;
+if(c < words.length() && c % 2 == 0){
+	System.out.println(words.charAt(c));
+	}c++;
+
+
+
+
+}
+}
