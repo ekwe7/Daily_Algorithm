c15c80781757a0385394a1f0d9438adec0db7d37
ekwe7 <ekwea753@gmail.com>
2025-12-26 15:25:25 +0100

    two perfect numbers that if added will give you target number

---
+++ java/TwoPerfectNumber.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoPerfectNumber {
+    public void main(String... ekwe){
+        int[] numbers = {2, 11, 15, 4, 5, 7};
+        int target = 9;
+
+        int[] result = twoPerfectNumber(numbers, target);
+        System.out.println(Arrays.toString(result));
+    }
+
+
+    public int[] twoPerfectNumber(int[] numbers, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int index = 0; index < numbers.length; index++) {
+            int numberMatch = target - numbers[index];
+
+            if (map.containsKey(numberMatch)) {
+                return new int[]{map.get(numberMatch), index};
+            }
+            map.put(numbers[index], index);
+        }
+    return new int[] {};
+    }
+}
